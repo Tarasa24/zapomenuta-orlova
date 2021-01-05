4e0212103f34707f2d80549a3e371d8de92cb3d2
Tarasa24 <petr.gajdosik2424@gmail.com>
2021-01-05 23:32:15 +0100

    moved root styles and added background gradient

--- src/App.vue
+++ src/App.vue
@@ -19,3 +19,17 @@ export default {
   },
 }
 </script>
+
+
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+
+body
+  margin: 0
+  background: $grey
+  background-image: linear-gradient(90deg, rgba($grey,1) 0%, rgba(255,255,255,1) 15%, rgba(255,255,255,1) 85%, rgba($grey,1) 100%), linear-gradient(0deg, rgba($grey,1) 0%, rgba(255,255,255,1) 15%) 
+</style>
